33a25ba2bedb99a87abc7f89907c897a839e9840
Javier Santiago <javiersantiago45011@gmail.com>
2024-09-04 08:33:49 -0500

    Base commit

---
+++ src/main/java/com/pragma/emazon_user/EmazonUserApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.emazon_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmazonUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmazonUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/emazon_user/EmazonUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.emazon_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
